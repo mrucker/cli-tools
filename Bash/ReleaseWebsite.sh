570851ec9612f65d890e572dded7720eb56667e4
EC2 Default User <ec2-user@ip-172-31-57-202.ec2.internal>
2015-08-15 21:21:44 +0000

    Fixing bugs with my deployment script

--- Bash/ReleaseWebsite.sh
+++ Bash/ReleaseWebsite.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#This has been tested against aws cli 1.7.45 and jekyll 2.5.3
+
 src="$1"
 dst="$2"
 bkt="$3"
@@ -14,22 +16,23 @@ green () {
 }
 
 green 'Jekyll build...'
-jekyll build -s $src -d $dst
+jekyll build -s $src -d "$dst"
 
 green 'Compress files...'
-find $dst \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -nv {} \; -exec mv {}.gz {} \;
+find "$dst" \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -nv {} \; -exec mv {}.gz {} \;
 
 green 'Sync css...'
-aws s3 sync $dst $bkt --size-only --exclude '*' --include '*.css' --recursive --delete --cache-control "$cacheControl" --conent-type 'text/css' --content-encoding 'gzip' 
+aws s3 sync "$dst" "$bkt" --size-only --exclude "*" --include "*.css" --delete --cache-control "$cacheControl" --content-type "text/css" --content-encoding "gzip" 
 
 green 'Sync js...'
-aws s3 sync $dst $bkt --size-only --exclude '*' --include '*.js' --recursive --delete --cache-control "$cacheControl" --conent-type 'application/javascript' --content-encoding 'gzip'
+aws s3 sync "$dst" "$bkt" --size-only --exclude '*' --include '*.js' --delete --cache-control "$cacheControl" --content-type 'application/javascript' --content-encoding 'gzip'
 
 green 'Sync html...'
-aws s3 sync $dst $bkt --size-only --exclude '*' --include '*.html' --recursive --delete --cache-control "$cacheControl" --conent-type 'text/html' --content-encoding 'gzip'
+aws s3 sync "$dst" "$bkt" --size-only --exclude '*' --include '*.html' --delete --cache-control "$cacheControl" --content-type 'text/html' --content-encoding 'gzip'
 
 green 'Sync images...'
-aws s3 sync $dst $bkt --size-only --exclude '*' --include '*.jpg' --include '*.png' --incude '*.ico' --recursive --delete --cache-control "$cacheControl"
+aws s3 sync "$dst" "$bkt" --size-only --exclude '*' --include '*.jpg' --include '*.png' --include '*.ico' --delete --cache-control "$cacheControl"
+
 
 green 'Sync all other files...'
-aws s3 sync $dst $bkt --size-only --recursive --delete
+aws s3 sync "$dst" "$bkt" --size-only --delete
