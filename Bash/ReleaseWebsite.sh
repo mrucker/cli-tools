85eebea2a04999a55c6e5037ad090cf3699d4cf3
Mark Rucker <rucker.mark@gmail.com>
2016-11-02 23:45:04 +0000

    Making my script path with spaces friendly

--- Bash/ReleaseWebsite.sh
+++ Bash/ReleaseWebsite.sh
@@ -16,10 +16,10 @@ green () {
 }
 
 green 'Jekyll build...'
-jekyll build -s $src -d "$dst"
+jekyll build -s "$src" -d "$dst"
 
 green 'Compress files...'
-find "$dst" \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -nv {} \; -exec mv {}.gz {} \;
+find "$dst" \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -nv "{}" \; -exec mv "{}.gz" "{}" \;
 
 green 'Sync css...'
 aws s3 sync "$dst" "$bkt" --size-only --exclude "*" --include "*.css" --delete --cache-control "$cacheControl" --content-type "text/css" --content-encoding "gzip" 
