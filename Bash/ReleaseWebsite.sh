9aea12ae591146246c92dcb3f35e0c668b1a71a7
Mark Rucker <rucker.mark@gmail.com>
2020-05-08 11:13:05 -0400

    improving my release script to reduce verbosity and no-cache html

--- Bash/ReleaseWebsite.sh
+++ Bash/ReleaseWebsite.sh
@@ -18,21 +18,20 @@ green () {
 green 'Jekyll build...'
 jekyll build -s "$src" -d "$dst"
 
-green 'Compress files...'
-find "$dst" \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -nv "{}" \; -exec mv "{}.gz" "{}" \;
+green 'Compressing files...'
+find "$dst" \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -n "{}" \; -exec mv "{}.gz" "{}" \;
 
-green 'Sync css...'
-aws s3 sync "$dst" "$bkt" --size-only --exclude "*" --include "*.css" --delete --cache-control "$cacheControl" --content-type "text/css" --content-encoding "gzip" 
+green 'Syncing css...'
+aws s3 sync "$dst" "$bkt" --exclude '*' --include '*.css*' --delete --cache-control "$cacheControl" --content-type "text/css" --content-encoding "gzip" --quiet
 
-green 'Sync js...'
-aws s3 sync "$dst" "$bkt" --size-only --exclude '*' --include '*.js' --delete --cache-control "$cacheControl" --content-type 'application/javascript' --content-encoding 'gzip'
+green 'Syncing js...'
+aws s3 sync "$dst" "$bkt" --exclude '*' --include '*.js' --delete --cache-control "$cacheControl" --content-type 'application/javascript' --content-encoding 'gzip' --quiet
 
-green 'Sync html...'
-aws s3 sync "$dst" "$bkt" --size-only --exclude '*' --include '*.html' --delete --cache-control "$cacheControl" --content-type 'text/html' --content-encoding 'gzip'
+green 'Syncing html...'
+aws s3 sync "$dst" "$bkt" --exclude '*' --include '*.html' --delete --cache-control 'no-cache' --content-type 'text/html' --content-encoding 'gzip' --quiet
 
-green 'Sync images...'
-aws s3 sync "$dst" "$bkt" --size-only --exclude '*' --include '*.jpg' --include '*.png' --include '*.ico' --delete --cache-control "$cacheControl"
+green 'Syncing images...'
+aws s3 sync "$dst" "$bkt" --exclude '*' --include '*.jpg' --include '*.png' --include '*.ico' --delete --cache-control "$cacheControl" --quiet
 
-
-green 'Sync all other files...'
-aws s3 sync "$dst" "$bkt" --size-only --delete
+green 'Syncing all other files...'
+aws s3 sync "$dst" "$bkt" --exclude '*.css*' --exclude '*.js' --exclude '*.html' --exclude '*.jpg' --exclude '*.png' --exclude '*.ico'  --exact-timestamps --delete --quiet
