662ce376d4b3537c00c593e8155eb1ef8253a0fd
Mark Rucker <rucker.mark@gmail.com>
2020-08-14 11:25:12 -0400

    better handling of vs code and default directory

--- PowerShell/Modules/base/base.psm1
+++ PowerShell/Modules/base/base.psm1
@@ -2,15 +2,34 @@ $PROJECTS = "$HOME\Projects"
 $TOOLS    = "$HOME\Tools"
 $DESKTOP  = "$HOME\Desktop"
 
-cd $PROJECTS
+$specifyDirCommands  = '\b(Set-Location|sl|cd|chdir|Push-Location|pushd|pul)\b'
+$specifyDirArguments ='^-(WorkingDirectory|wd|wo|wor|work)'
 
-$ENV:PATH += "$TOOLS\cURL\bin;"
+$dirSpecifiedByCommand  = [Environment]::CommandLine -match $set_dir_comands
+$dirSpecifiedByArgument = $PSVersionTable.PSEdition -ne 'Desktop' -and [Environment]::GetCommandLineArgs() -match $set_dir_arguments
+
+$dirSpecified = $dirSpecifiedByCommand -or $dirSpecifiedByArgument
+
+#for $Env:vscode you must manually add "vscode" to
+#terminal.integrated.env.windows in workspace or 
+#user vscode settings
+if (-not $dirSpecified -and -not $Env:vscode) {
+    cd $PROJECTS
+}
+
+$ENV:PATH += ";"
 $ENV:PATH += "$TOOLS\7-Zip\Application\;"
+$ENV:PATH += "$TOOLS\hugo\;"
 
 New-Alias ex C:\Windows\explorer.exe
 New-Alias n++ "$TOOLS\Notepad++\Notepad++.exe"
 
 Set-PSReadlineOption -BellStyle None
+
+#this assumes that bash for windows is enabled
+function vim ($file){
+	bash -c "vim -c 'setlocal ff=dos' $($file -replace '\\', '/' -replace ' ', '\ ')"
+}
 
 function Prompt {
 	Write-Host "PS" -NoNewline
@@ -20,4 +39,4 @@ function Prompt {
 	return " "
 }
 
-Export-ModuleMember -function Prompt -Alias ex, n++ -Variable PROJECTS, TOOLS, DESKTOP
+Export-ModuleMember -function Prompt, vim -Alias ex, n++ -Variable PROJECTS, TOOLS, DESKTOP
