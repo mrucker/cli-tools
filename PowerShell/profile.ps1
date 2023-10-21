6ab005e7490b7c7ff7dd22f458fdf3a3045589a2
Mark Rucker <rucker.mark@gmail.com>
2023-10-21 15:14:18 -0400

    Updated default ps1 profile.

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,1 +1,32 @@
-Import-Module 'base'
+$PROJECTS = "$HOME\Projects"
+$DESKTOP  = "$HOME\Desktop"
+
+$specifyDirCommands  = '\b(Set-Location|sl|cd|chdir|Push-Location|pushd|pul)\b'
+$specifyDirArguments ='^-(WorkingDirectory|wd|wo|wor|work)'
+
+$dirSpecifiedByCommand  = [Environment]::CommandLine -match $specifyDirCommands
+$dirSpecifiedByArgument = $PSVersionTable.PSEdition -ne 'Desktop' -and [Environment]::GetCommandLineArgs() -match $specifyDirArguments
+
+$dirSpecified = $dirSpecifiedByCommand -or $dirSpecifiedByArgument
+
+$Env:Path += ";$HOME\php"
+
+New-Alias ex "C:\Windows\explorer.exe"
+New-Alias n++ "C:\Program Files\Notepad++\notepad++.exe"
+
+Set-PSReadlineOption -BellStyle None
+
+function Prompt {
+	Write-Host "PS" -NoNewline
+	Write-Host " "  -NoNewline
+	Write-Host $(pwd).Path -ForegroundColor Cyan  -NoNewline
+	Write-Host ">"         -ForegroundColor White -NoNewline
+	return " "
+}
+
+#for $Env:vscode you must manually add "vscode" to
+#terminal.integrated.env.windows in workspace or 
+#user vscode settings
+if (-not $dirSpecified -and -not $Env:vscode) {
+    cd $PROJECTS
+}
